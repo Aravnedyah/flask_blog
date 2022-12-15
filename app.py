88060ff47a6e494f453d73de841954ccf9519f51
Aravnedyah <74086054+Aravnedyah@users.noreply.github.com>
2022-12-15 13:30:07 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
+#connect to created db and convert to dictionary rows and returned
 def get_db_connection():
     conn=sqlite3.connect('database.db')
     conn.row_factory=sqlite3.Row
     return conn
 
+#receive an id parameter, call db, post new data. If no post, call 404 error and return post
 def get_post(post_id):
     conn=get_db_connection()
     post=conn.execute('SELECT * FROM posts WHERE id=?',
@@ -16,9 +18,11 @@ def get_post(post_id):
         abort(404)
     return post
 
+#initiate app and create secret key
 app = Flask(__name__)
 app.secret_key= 'sdfasdf9sdf0sdf909f0d9fsdfs-098765456#@$@#$(@*dfsdfjsdhfksdfjsdkfew3412iiacuwer12endi12edbasd'
 
+# @app.route() is directing the view. call db, get all posts and return them.
 @app.route('/')
 def index():
     conn = get_db_connection()
@@ -26,16 +30,18 @@ def index():
     conn.close()
     return render_template('index.html', posts=posts)
 
+#view directs to about page. renders about page
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-
+#generates all posts on index page
 @app.route('/<int:post_id>')
 def post(post_id):
     post=get_post(post_id)
     return render_template('post.html', post=post)
 
+#viewer directs to create.html 
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
